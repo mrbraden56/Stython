cbf624d621e799efd8e293cc0054ca88e885bf44
mrbaden56 <bradenlock83@gmail.com>
2024-03-27 20:05:35 -0500

    memory mapped file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <string>
 
-int main(){
-	std::cout<<"Hello World!!\n";
+#include "FileMap.cpp"
 
-	return 0;
+int main() {
+  const char *stython_program_path = "example_program.sy";
+  FileMap file(stython_program_path);
+
+  if (!file) {
+    printf("Error: failed to open file\n");
+    return 1;
+  }
+
+  /*   printf("Source: %.*s\n", file.Size(), file.Begin()); */
+  /*   Scanner scanner(file.Begin(), file.End()); */
+  for (const char *p = file.Begin(); p != file.End(); ++p) {
+    std::cout << *p; // Print each character
+  }
+
+  return 0;
 }
