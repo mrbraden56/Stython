7f0faabe9f6ba61ca447e1901d4e8eac4f32f006
mrbaden56 <bradenlock83@gmail.com>
2024-04-05 08:03:00 -0500

    Finished scanner; need to add errors and tests

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 build:
-	g++ main.cpp -std=c++11 -o out
+	g++ -g main.cpp -std=c++11 -o out
--- Scanner.h
+++ Scanner.h
@@ -10,18 +10,22 @@
 
 using namespace std;
 
-class Scanner {
+class Scanner
+{
 
 public:
   Scanner(const std::string &fileContent) : source_code(fileContent) {}
 
-  void generateTokens() {
-    while (!atEnd()) {
+  void generateTokens()
+  {
+    while (!atEnd())
+    {
+      start = current;
       scanToken();
     }
-    for (Token &token : Tokens) {
-
-      // token.printToken();
+    for (Token &token : Tokens)
+    {
+      token.printToken();
     }
   }
 
@@ -34,18 +38,24 @@ private:
   int start = 0;   // Used to track start of the token
   int current = 0; // Used to track where we currently are
   unordered_map<string, TokenKind> reservedWords = {
-      {"if", TokenKind::IF},         {"else", TokenKind::ELSE},
-      {"for", TokenKind::FOR},       {"return", TokenKind::RETURN},
-      {"number", TokenKind::NUMBER}, {"string", TokenKind::STRING},
-      {"def", TokenKind::DEF},       {"void", TokenKind::VOID},
-      {"break", TokenKind::BREAK},   {"continue", TokenKind::CONTINUE},
+      {"if", TokenKind::IF},
+      {"else", TokenKind::ELSE},
+      {"for", TokenKind::FOR},
+      {"return", TokenKind::RETURN},
+      {"number", TokenKind::NUMBER},
+      {"string", TokenKind::STRING},
+      {"def", TokenKind::DEF},
+      {"void", TokenKind::VOID},
+      {"break", TokenKind::BREAK},
+      {"continue", TokenKind::CONTINUE},
       {"const", TokenKind::CONST},
 
   };
 
   bool atEnd() { return current >= source_code.length(); }
 
-  char advance() {
+  char advance()
+  {
     char c = source_code[current];
     current++;
     return c;
@@ -53,142 +63,161 @@ private:
 
   void addToken(TokenKind token) {}
 
-  bool lookOneAhead(char match) {
-    if (source_code[current + 1] == match) {
+  bool lookOneAhead(char match)
+  {
+    if (source_code[current] == match)
+    {
       return true;
-    } else {
+    }
+    else
+    {
       return false;
     }
   }
 
-  bool isAlpha(char c) {
+  bool isAlpha(char c)
+  {
     return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
   }
 
   // Checks if a character is an alphabet letter, underscore, or digit
-  bool isAlphaNumeric(char c) {
+  bool isAlphaNumeric(char c)
+  {
     return isAlpha(c) || isdigit(static_cast<unsigned char>(c));
   }
 
-  void stringLiteral() {
-    advance();
+  void stringLiteral()
+  {
     start = current;
-    while (source_code[current] != '"' && !atEnd()) {
+    while (source_code[current] != '"' && !atEnd())
+    {
+    string val = source_code.substr(start, (current)-start + 1);
       advance();
     }
-    // NOTE: Now start is at the begening " and current is at the end "
-    string val = source_code.substr(start, current);
-
+    string temp = source_code.substr(start, (current)-start);
     Tokens.push_back(StringLiteralToken(
-        TokenKind::STRING, source_code.substr(start, (current)-start + 1)));
+        TokenKind::STRING, source_code.substr(start, (current)-start)));
+    advance();
   }
 
-  void numberLiteral() {
-    start = current;
+  void numberLiteral()
+  {
     while (isdigit(static_cast<unsigned char>(source_code[current])) ||
-           source_code[current] == '.' && !atEnd()) {
+           source_code[current] == '.' && !atEnd())
+    {
       advance();
     }
-
-    string val = source_code.substr(start, current);
 
     Tokens.push_back(NumberLiteralToken(
-        TokenKind::NUMBER, source_code.substr(start, (current)-start + 1)));
+        TokenKind::NUMBER, source_code.substr(start, (current)-start)));
   }
 
-  void identifier() {
-    start = current;
-    while (isAlpha(source_code[current]) && !atEnd()) {
+  void identifier()
+  {
+    while (isAlpha(source_code[current]) && !atEnd())
+    {
       advance();
     }
+    string debug_temp = source_code.substr(start, (current)-start);
 
-    if (reservedWords.find(source_code.substr(start, current)) !=
-        reservedWords.end()) {
+    if (reservedWords.find(source_code.substr(start, (current)-start)) !=
+        reservedWords.end())
+    {
       Tokens.push_back(
           KeywordToken(reservedWords[source_code.substr(start, current)],
-                       source_code.substr(start, (current)-start + 1)));
-    } else {
+                       source_code.substr(start, (current)-start)));
+    }
+    else
+    {
       Tokens.push_back(
           IdentifierToken(TokenKind::IDENTIFIER,
-                          source_code.substr(start, (current)-start + 1)));
+                          source_code.substr(start, (current)-start)));
     }
   }
 
-  void scanToken() {
+  void scanToken()
+  {
     char c = advance();
-    switch (c) {
+    switch (c)
+    {
     case '(':
       Tokens.push_back(SpecialSymbolToken(TokenKind::LEFT_PAREN,
-                                          source_code.substr(current, 1)));
+                                          source_code.substr(start, 1)));
       break;
     case ')':
       Tokens.push_back(SpecialSymbolToken(TokenKind::RIGHT_PAREN,
-                                          source_code.substr(current, 1)));
+                                          source_code.substr(start, 1)));
       break;
     case '{':
       Tokens.push_back(SpecialSymbolToken(TokenKind::LEFT_BRACE,
-                                          source_code.substr(current, 1)));
+                                          source_code.substr(start, 1)));
       break;
     case '}':
       Tokens.push_back(SpecialSymbolToken(TokenKind::RIGHT_BRACE,
-                                          source_code.substr(current, 1)));
+                                          source_code.substr(start, 1)));
       break;
     case ',':
       Tokens.push_back(
-          SpecialSymbolToken(TokenKind::COMMA, source_code.substr(current, 1)));
+          SpecialSymbolToken(TokenKind::COMMA, source_code.substr(start, 1)));
       break;
     case ';':
       Tokens.push_back(SpecialSymbolToken(TokenKind::SEMICOLON,
-                                          source_code.substr(current, 1)));
+                                          source_code.substr(start, 1)));
       break;
     case '-':
-      cout << c << "\n";
-      Tokens.push_back(
-          lookOneAhead('>')
-              ? OperatorToken(TokenKind::RETURN_SYMBOL,
-                              source_code.substr(start, (current)-start + 1))
-              : OperatorToken(TokenKind::MINUS,
-                              source_code.substr(current, 1)));
+      if (lookOneAhead('>'))
+      {
+        current++; // Increment current if lookOneAhead is true
+        Tokens.push_back(
+            OperatorToken(TokenKind::RETURN_SYMBOL, source_code.substr(start, (current)-start)));
+      }
+      else
+      {
+        Tokens.push_back(
+            OperatorToken(TokenKind::MINUS, source_code.substr(start, 1)));
+      }
+
       break;
     case '+':
       Tokens.push_back(
-          OperatorToken(TokenKind::PLUS, source_code.substr(current, 1)));
+          OperatorToken(TokenKind::PLUS, source_code.substr(start, 1)));
       break;
     case '/':
       Tokens.push_back(
-          OperatorToken(TokenKind::SLASH, source_code.substr(current, 1)));
+          OperatorToken(TokenKind::SLASH, source_code.substr(start, 1)));
       break;
     case '*':
       Tokens.push_back(
-          OperatorToken(TokenKind::STAR, source_code.substr(current, 1)));
+          OperatorToken(TokenKind::STAR, source_code.substr(start, 1)));
       break;
     case '<':
       Tokens.push_back(
           lookOneAhead('=')
               ? OperatorToken(TokenKind::LESS_EQUAL,
-                              source_code.substr(current, 1))
-              : OperatorToken(TokenKind::LESS, source_code.substr(current, 1)));
+                              source_code.substr(start, (current)-start + 1))
+              : OperatorToken(TokenKind::LESS, source_code.substr(start, 1)));
       break;
     case '>':
-      Tokens.push_back(lookOneAhead('=')
-                           ? OperatorToken(TokenKind::GREATER_EQUAL,
-                                           source_code.substr(current, 1))
-                           : OperatorToken(TokenKind::GREATER,
-                                           source_code.substr(current, 1)));
+      Tokens.push_back(
+          lookOneAhead('=')
+              ? OperatorToken(TokenKind::GREATER_EQUAL,
+                              source_code.substr(start, (current)-start + 1))
+              : OperatorToken(TokenKind::GREATER,
+                              source_code.substr(start, 1)));
       break;
     case '=':
-      Tokens.push_back(lookOneAhead('=')
-                           ? OperatorToken(TokenKind::EQUAL_EQUAL,
-                                           source_code.substr(current, 1))
-                           : OperatorToken(TokenKind::EQUAL,
-                                           source_code.substr(current, 1)));
+      Tokens.push_back(
+          lookOneAhead('=')
+              ? OperatorToken(TokenKind::EQUAL_EQUAL,
+                              source_code.substr(start, (current)-start + 1))
+              : OperatorToken(TokenKind::EQUAL, source_code.substr(start, 1)));
       break;
     case '!':
       Tokens.push_back(
           lookOneAhead('=')
               ? OperatorToken(TokenKind::BANG_EQUAL,
-                              source_code.substr(current, 1))
-              : OperatorToken(TokenKind::BANG, source_code.substr(current, 1)));
+                              source_code.substr(start, (current)-start + 1))
+              : OperatorToken(TokenKind::BANG, source_code.substr(start, 1)));
       break;
     case ' ':
       // Handling for space character, presumably skipping it.
@@ -200,12 +229,14 @@ private:
       break;
     default:
       // NOTE: Number Literals
-      if (isdigit(static_cast<unsigned char>(c))) {
+      if (isdigit(static_cast<unsigned char>(c)))
+      {
         numberLiteral(); // Implementation presumably handles number literals
       }
 
       // NOTE: Reserved words and identifiers
-      if (isalpha(static_cast<unsigned char>(c))) {
+      if (isalpha(static_cast<unsigned char>(c)))
+      {
         identifier(); // Implementation presumably handles identifiers
       }
     }
